69ff77de326cf2ea7bc5ddf7960d44e87ed4782f
zhufengnodejs <zhufengnodejs@126.com>
2016-07-28 16:37:49 +0800

    ajax

---
+++ js/11.ajax.js
@@ -0,0 +1,33 @@
+var Suggestion = React.createClass({
+    getInitialState(){
+      return {content:[]};
+    },
+    handleChange(event){
+        var keyword = event.target.value;
+        $.ajax({
+            url:'http://www.baidu.com/su',
+            method:'get',
+            jsonp:'cb',//指定后台用来接收函数名的查询字符串参数值
+            dataType:'jsonp',//告诉jquery如何处理响应xt
+            context:this,
+            data:{wd:keyword},
+            success:function(data){
+                var result = data.s;
+                var lis = result.map(function(item){
+                    return <li>{item}</li>
+                })
+                this.setState({content:lis});
+            }
+        })
+    },
+    render(){
+        return <div>
+            <input type="text" onChange={this.handleChange}/><br/>
+            <ul>
+                {this.state.content}
+            </ul>
+        </div>
+    }
+});
+
+ReactDOM.render(<Suggestion/>,document.querySelector('#app'));
