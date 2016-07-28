b7dd7294ec2993e8376512e3b9e949911f6e1910
zhufengnodejs <zhufengnodejs@126.com>
2016-07-28 17:28:00 +0800

    自己实现ajax

--- js/11.ajax.js
+++ js/11.ajax.js
@@ -2,10 +2,44 @@ var Suggestion = React.createClass({
     getInitialState(){
       return {content:[]};
     },
+    ajax(options){
+        var url = options.url;
+        if(options.data){
+            var query = '';
+            for(var attr in options.data){
+                query+= (attr+'='+options.data[attr])+'&';
+            }
+            // query = wd=a&
+            url+=((/\?/.test(url)?'&':'?')+query);
+        }
+        url += ('_='+Date.now());
+        if(options.jsonp){
+            url+=('&'+options.jsonp+'=show');
+        }
+        if(options.context){
+            options.success = options.success.bind(options.context);
+        }
+        var xhr = new XMLHttpRequest;
+        xhr.open(options.method,url,true);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState==4 && xhr.status== 200){
+                var result = xhr.responseText;
+                if(options.dataType=='json'){
+                    result = JSON.parse(result);
+                }else if(options.dataType=='jsonp'){
+                    var res = result.match(/\w+\((.+)\)/);
+                    res = res[1];
+                    result = JSON.parse(res);
+                }
+                options.success(result);
+            }
+        }
+        xhr.send();
+    },
     handleChange(event){
         var keyword = event.target.value;
-        $.ajax({
-            url:'http://www.baidu.com/su',
+        this.ajax({
+            url:'http://localhost:8080/su',
             method:'get',
             jsonp:'cb',//指定后台用来接收函数名的查询字符串参数值
             dataType:'jsonp',//告诉jquery如何处理响应xt
@@ -13,8 +47,8 @@ var Suggestion = React.createClass({
             data:{wd:keyword},
             success:function(data){
                 var result = data.s;
-                var lis = result.map(function(item){
-                    return <li className="list-group-item">{item}</li>
+                var lis = result.map(function(item,index){
+                    return <li className="list-group-item" key={index}>{item}</li>
                 })
                 this.setState({content:lis});
             }
